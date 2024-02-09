eaa10f46393e5d509c93aa2bd5706775df0f621b
Frederick Morrison <frederick.morrison@outlook.com>
2024-02-09 08:28:57 -0700

    echo main directory location to console

--- aliens.py
+++ aliens.py
@@ -49,11 +49,11 @@ MAX_SHOTS = 2  # most player bullets onscreen
 ALIEN_ODDS = 22  # chances a new alien appears
 BOMB_ODDS = 60  # chances a new bomb will drop
 ALIEN_RELOAD = 12  # frames between new aliens
-# SCREENRECT = pg.Rect(0, 0, 640, 480)
 SCREENRECT = pg.Rect(0, 0, settings.screen_width, settings.screen_height)
 SCORE = 0
 
 main_dir = os.path.split(os.path.abspath(__file__))[0]
+print(f'main directory: {main_dir}')
 
 
 def load_image(file):
